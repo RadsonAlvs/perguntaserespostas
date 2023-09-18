6a578820e164d5025e6fe131d3e0d5ccd0fff44e
radson.lima <radson.lima@grupoicts.com.br>
2023-09-18 12:54:31 -0300

    finalizando

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const connection = require('./database/database')
 const Pergunta = require('./database/Pergunta');
+const Resposta = require('./database/Resposta')
 
 // Database
 connection
@@ -24,7 +25,11 @@ app.use(bodyParser.json());
 
 // Rotas
 app.get('/', (req, res) => {
-  res.render('index');
+  Pergunta.findAll({ raw: true, order: [['id','DESC']] }).then((perguntas) => {
+    res.render('index', {
+      perguntas: perguntas
+    });
+  });
 });
 
 app.get('/perguntar', (req, res) => {
@@ -39,6 +44,40 @@ app.post('/salvarpergunta', (req, res) => {
     descricao: descricao
   }).then(() => {
     res.redirect('/')
+  })
+});
+
+app.get('/pergunta/:id', (req, res) => {
+  var id = req.params.id;
+  Pergunta.findOne({
+     where:{ id: id },
+     order: ['id','DESC']
+    })
+    .then(pergunta => {
+      if(pergunta != undefined){
+
+        Resposta.findAll({
+          where: {perguntaId: pergunta.id}
+        }).then(respostas => {
+          res.render('pergunta',{
+            pergunta: pergunta,
+            respostas: respostas
+          })
+        })
+      }else{
+        res.redirect('/')
+      }
+    });
+});
+
+app.post('/responder', (req,res) => {
+  var corpo = req.body.corpo;
+  var perguntaId = req.body.pergunta;
+  Resposta.create({
+    corpo: corpo,
+    perguntaId: perguntaId
+  }).then(() => {
+    res.redirect('/pergunta/'+perguntaId)
   })
 });
 
